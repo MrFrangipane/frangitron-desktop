2ce3245ece1094aacaba40e1478a0b0f3bc1a9f1
MrFrangipane <mister@frangipane.me>
2019-03-03 18:24:10 +0100

    fix push and compile arguments

--- frangitron/desktop_computer.py
+++ frangitron/desktop_computer.py
@@ -5,8 +5,10 @@ def push_and_compile(commit_message):
     """
     Executes shell script 'build.sh' from `git:frangitron-raspberry/scripts`
     """
+    command = ['gnome-terminal', '-x', 'bash', '-c', './push.sh "{}"'.format(commit_message)]
+
     subprocess.Popen(
-        ['gnome-terminal', '-x', 'bash', '-c', './push.sh', commit_message],
+        command,
         cwd="/home/frangi/frangitron/frangitron-raspberry/scripts",
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT
